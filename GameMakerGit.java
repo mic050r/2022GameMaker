7f4291567f1f6b9860f4f1eede0f4ba345c4d7ef
mic050r <103114387+mic050r@users.noreply.github.com>
2022-04-13 17:26:43 +0900

    Update GameMakerGit.java

--- GameMakerGit.java
+++ GameMakerGit.java
@@ -1,7 +1,7 @@
 public class GamemakerGit{
 	public static void main(String args[]){
 		System.out.println("Hello Gamemaker");
-		System.out.println("���� ����Ŀ ȭ����");
+		System.out.println("게임 메이커 화이팅!");
 
 	}
 }
